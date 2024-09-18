448e942ccb938141413840772b73ed5f19a73343
lljamesnll <funnyoohoohaahaah@gmail.com>
2024-09-18 13:47:31 +1000

    Add project files.

---
+++ LookCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration4
+{
+    public class LookCommand : Command
+    {
+        public LookCommand() : base(new string[] { "look" })
+        {
+
+        }
+
+        public override string Execute(Player p, string[] text)
+        {
+            if (text.Length == 5 || text.Length == 3)
+            {
+                if (text[0] == "look")
+                {
+                    if (text[1] == "at")
+                    {
+                        if (text.Length == 5)
+                        {
+                            if (text[3] == "in")
+                            {
+                                if (FetchContainer(p, text[4]) != null)
+                                {
+                                    return LookAtIn(text[2], FetchContainer(p, text[4]));
+                                }
+                                return "I can't find the " + text[4];
+                            }
+                            return "What do you want to look in?";
+                        }
+                        else
+                        {
+                            if (p.Locate(text[2]) != null)
+                            {
+                                return p.Locate(text[2]).FullDescription;
+                            }
+                            return "I can't find the " + text[2];
+                        }
+                    }
+                    return "What do you want to look at?";
+                }
+                return "Error in look input";
+            }
+            return "I don't know how to look at that.";
+        }
+
+        private IHaveInventory FetchContainer(Player p, string containerId)
+        {
+            return p.Locate(containerId) as IHaveInventory;
+        }
+
+        private string LookAtIn(string thingId, IHaveInventory container)
+        {
+            if (container.Locate(thingId) != null)
+            {
+                return container.Locate(thingId).FullDescription;
+            }
+            return "I can't find the " + thingId;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string player_name = Console.ReadLine();
+            Console.WriteLine("Tell me about yourself: ");
+            string player_desc = Console.ReadLine();
+            Player player = new Player(player_name, player_desc);
+            Item nuke = new Item(new String[] { "nuke", "nuclear_bomb" }, "nuke", "a literal nuke, could wipe out a city.");
+           
+            Item golf_club = new Item(new String[] { "golf_club", "driver" }, "golf club", "a golf club, perfect for getting birdies.");
+           
+            Item cancer = new Item(new String[] { "cancer", "leukemia" }, "cancer", "a debilitating disease.");
+            player.Inventory.Put(nuke);
+            player.Inventory.Put(golf_club);
+            Bag plastic_bag = new Bag(new string[] { "plastic_bag", "aldi_bag" }, "plastic bag", "a bag that you got from woolworths.");
+            plastic_bag.Inventory.Put(cancer);
+            player.Inventory.Put(plastic_bag);
+            bool continue_loop = true;
+            while (continue_loop)
+            {
+                LookCommand lookcommand = new LookCommand();
+                Console.WriteLine(lookcommand.Execute(player,
+               Console.ReadLine().Split(' ')));
+                Console.WriteLine("Would you like to continue with other commands? (y / n)");
+            if (Console.ReadLine() == "n")
+                {
+                    continue_loop = false;
+                }
+            }
+        }
+    }
+}
